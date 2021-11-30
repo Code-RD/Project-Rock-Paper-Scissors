702591e77d39c80f124e353c4046a2cbde9880cd
Code-RD <engel.rosario@gmail.com>
2021-11-29 21:16:30 -0500

    Update file to current state

---
+++ functions.js
@@ -0,0 +1,11 @@
+
+// let option = "MySelection";
+// document.getElementById("Rock").value = option;
+var user;
+function choose(choice){
+    user = choice;
+}
+
+function test(click){
+    alert("You chose " + user);
+}      
